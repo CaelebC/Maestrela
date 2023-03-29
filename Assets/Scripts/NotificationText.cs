a336d042eb0f38b84d4a25b7dae31e059893b42f
CaelebC <caelebcamaro1@gmail.com>
2023-03-30 00:19:18 +0800

    Shows message when higher tier mobs/final wave is about to happen.

--- Assets/Scripts/NotificationText.cs
+++ Assets/Scripts/NotificationText.cs
@@ -6,6 +6,10 @@ using TMPro;
 public class NotificationText : MonoBehaviour
 {
     [SerializeField] float notifDuration = 1.5f;
+    [SerializeField] float msgDuration = 3.5f;
+
+    [SerializeField] [TextArea(3, 10)] string higherTierEnemyIncomingMsg;
+    [SerializeField] [TextArea(3, 10)] string lastWaveIncomingMsg;
     
     [Header("Unity Setup Fields")]
     [SerializeField] TextMeshProUGUI textObject;
@@ -16,12 +20,14 @@ public class NotificationText : MonoBehaviour
     {
         LoadoutManager.GiveNotif += ShowNotif;
         Node.OnBuildError += ShowNotif;
+        WaveSpawner.OnNewWave += ShowMsg;
     }
 
     void OnDestroy()
     {
         LoadoutManager.GiveNotif -= ShowNotif;
         Node.OnBuildError -= ShowNotif;
+        WaveSpawner.OnNewWave -= ShowMsg;
     }
 
     void ShowNotif(string _notifContent)
@@ -37,6 +43,31 @@ public class NotificationText : MonoBehaviour
         TextAnim.SetBool("TextFadeOut", false);
 
         yield return new WaitForSeconds(notifDuration);
+
+        TextAnim.SetBool("TextFade", false);
+        TextAnim.SetBool("TextFadeOut", true);
+    }
+
+    void ShowMsg(int _waveIndex)
+    {
+        if (_waveIndex == 5 || _waveIndex == 12)
+        {
+            textObject.text = higherTierEnemyIncomingMsg;
+            StartCoroutine(FadingMsg());
+        }
+        if (_waveIndex == 18)
+        {
+            textObject.text = lastWaveIncomingMsg;
+            StartCoroutine(FadingMsg());            
+        }
+    }
+    
+    IEnumerator FadingMsg()
+    {
+        TextAnim.SetBool("TextFade", true);
+        TextAnim.SetBool("TextFadeOut", false);
+
+        yield return new WaitForSeconds(msgDuration);
 
         TextAnim.SetBool("TextFade", false);
         TextAnim.SetBool("TextFadeOut", true);
